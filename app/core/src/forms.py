e003d8bd6bc1cea1ec7b2a7d70fefc1450e25c6a
Laura Agudelo <lvagudelo19@misena.edu.co>
2020-06-03 22:18:58 -0500

    Actualización Proyecto

--- app/core/src/forms.py
+++ app/core/src/forms.py
@@ -6,5 +6,5 @@ class ContactForm(forms.Form):
     #Atributos del formulario de contacto 
     usuario = forms.CharField(label="Nombre", required=True, widget=forms.TextInput(attrs={'class':'formulario', 'placeholder':'Nombre'}))
     correo = forms.EmailField(label="Correo Electrónico", required=True,widget=forms.EmailInput(attrs={'class':'formulario','placeholder':'Correo Electrónico'}))
-    tipomsj = forms.ChoiceField(label="Asunto", required=True, choices=pqrsf, widget=forms.Select(attrs={'class','formulario'}))
+    tipomsj = forms.ChoiceField(label="Asunto", required=True, choices=pqrsf)
     mensaje = forms.CharField(label="Mensaje", required=True, widget=forms.Textarea(attrs={'class':'formulario', 'rows':'5','placeholder':'Escribe tu Mensaje'}))
--- app/core/src/views.py
+++ app/core/src/views.py
@@ -2,6 +2,7 @@ from django.views.generic.base import TemplateView
 from django.shortcuts import render, HttpResponse, redirect
 from django.urls import reverse
 from .forms import ContactForm
+from django.core.mail import EmailMessage
 # Create your views here.
 
 class HomePageView(TemplateView):
@@ -33,10 +34,28 @@ def contacto(request):
             formContact = ContactForm(data=request.POST)
             if formContact.is_valid():
                 usuario = request.POST.get('usuario','')
-                correo  = request.POST.get('correo ','')
+                correo = request.POST.get('correo ','')
                 tipomsj = request.POST.get('tipomsj','')
                 mensaje = request.POST.get('mensaje','')
-                return redirect(reverse('contacto')+"?ok")
+
+                #Objeto con las variables del formulario:
+                email = EmailMessage(
+                    "RepoDevelopers: tienes un nuevo mensaje de contacto",
+                    "De {} <{}>\n\nEscribió:\n\n{}".format(usuario, correo, mensaje),
+                    "no-contestar@inbox.mailtrap.io",
+                    ["lauragudelo102001@gmail.com"],
+                    reply_to=[correo]
+                )
+                #Enviamos el correo:
+                try:
+                    email.send()
+                    #se envia el correo:
+                    return redirect(reverse('contacto')+"?ok")
+                except:
+                    #No se ha enviado el correo:
+                    return redirect(reverse('contacto')+"?fail")
+
+                
 
         return render(request, 'contacto.html', {'formulario':formContact, 'btn1':'Inicio', 'btn2':'Nosotros', 'btn3':'Contacto', 'btn4': 'Presentación', 'txt1': '¡CONTÁCTATE CON NOSOTROS!', 'tituloContacto': '¡CONTÁCTANOS!', 'txt2':'Carrera 68 #104, Complejo Norte, Medellín, Antioquia', 'txt3':'sav_sena@correo.com', 'txt4':'3148867898', 'btn8':'Enviar', 'txt5':'SENA 2020 ', 'txt6':'Denisse Alejandra Alzate Meneses | Sergio León Saldarriaga Dávila | Laura Vanessa Agudelo Arias'} ) 
 
