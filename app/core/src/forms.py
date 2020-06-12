60a40531286a00b6539813945e7a0699f7a60991
Laura Agudelo <lvagudelo19@misena.edu.co>
2020-06-12 15:58:00 -0500

    Actualización

--- app/core/src/forms.py
+++ app/core/src/forms.py
@@ -4,7 +4,7 @@ from .pqrsf import pqrsf
 #Creamos las clases con los formularios
 class ContactForm(forms.Form):
     #Atributos del formulario de contacto 
-    usuario = forms.CharField(label="Nombre", required=True, widget=forms.TextInput(attrs={'class':'formulario', 'placeholder':'Nombre'}))
-    correo = forms.EmailField(label="Correo Electrónico", required=True,widget=forms.EmailInput(attrs={'class':'formulario','placeholder':'Correo Electrónico'}))
-    tipomsj = forms.ChoiceField(label="Asunto", required=True, choices=pqrsf)
-    mensaje = forms.CharField(label="Mensaje", required=True, widget=forms.Textarea(attrs={'class':'formulario', 'rows':'5','placeholder':'Escribe tu Mensaje'}))
+    usuario = forms.CharField(label="Nombre", required=True, widget=forms.TextInput(attrs={'class':'formulario input', 'placeholder':'Nombre'}))
+    correo = forms.EmailField(label="Correo Electrónico", required=True,widget=forms.EmailInput(attrs={'class':'formulario input','placeholder':'Correo Electrónico'}))
+    tipomsj = forms.ChoiceField(label="Asunto", required=True, choices=pqrsf, widget=forms.Select(attrs={'class':'formulario input'}))
+    mensaje = forms.CharField(label="Mensaje", required=True, widget=forms.Textarea(attrs={'class':'formulario input', 'rows':'5','placeholder':'Escribe tu Mensaje'}))
