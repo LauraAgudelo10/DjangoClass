1188c7e51fc93e30a24b06b4bedb5963f87f134e
Laura Agudelo <lvagudelo19@misena.edu.co>
2020-06-18 16:38:59 -0500

    Final Proyecto Python

--- app/core/src/views.py
+++ app/core/src/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.urls import reverse
 from .forms import ContactForm
 from django.core.mail import EmailMessage
+from django.views.generic import ListView
+from userdata.models import DatosUser
 # Create your views here.
 
 class HomePageView(TemplateView):
@@ -12,19 +14,31 @@ class HomePageView(TemplateView):
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {'btn1':'Inicio', 'btn2':'Sobre SAV', 'btn3':'Características', 'btn4':'Objetivo', 'btn5':'Nosotros', 'btn6':'Contacto', 'btn7': 'Presentación', 'tituloIni':'SISTEMA DE GESTIÓN VIRTUAL DE APRENDICES', 'tituloNosotros': 'Sobre SAV','spanUno': '1', 'tituloSAV':'SAV', 'txt1':'SAV es un aplicativo web dirigido al área de contrato de aprendizaje del SENA, este ayudará a los trabajadores de esta área a organizar una base de datos con información de aprendices que cuentan o no con una alternativa para la etapa productiva; con este aplicativo se podrá ver la información en conjunto como programa de Formación y/o ficha como también la información de un aprendiz en particular lo que brindará un apoyo más rápido y eficaz para la búsqueda de tal información a los usuarios.', 'tituloCaracteristicas': 'Características', 'tituloEficaz':'Eficaz', 'txt2': 'Adaptado para una buena búsqueda de la información.', 'tituloSeguro':'Seguro', 'txt3':'Cuida la información de cada uno de los usuarios permitiendo que sea un sitio web confiable.', 'tituloOrganizado':'Organizado', 'txt4': 'Diseño sencillo y modelado para una mayor facilidad de uso.', 'tituloObjetivo':'Objetivo', 'txt5':'Permitir administrar la información y la gestión documental pertinente a los procesos de seguimiento de alternativas al Contrato de Aprendizaje, tanto en la fase lectiva como en la fase productiva.', 'txt6':'SENA 2020 ', 'txt7':'Denisse Alejandra Alzate Meneses | Sergio León Saldarriaga Dávila | Laura Vanessa Agudelo Arias' })
 
-class NosotrosPageView(TemplateView):
+class NosotrosPageView(ListView):
 
+    model = DatosUser
     template_name = "nosotros.html"
 
-    def get(self,request, *args, **kwargs):
-       return render(request, self.template_name, {'btn1':'Inicio', 'btn2':'Nosotros', 'btn3':'Contacto', 'btn4': 'Presentación', 'texto':'¡CONOCE A NUESTRO EQUIPO DE DESARROLLADORES!', 'tituloNosotros':'Nosotros','integranteUno':'Denisse Alejandra', 'integranteDos':'Sergio León', 'integranteTres':'Laura Vanessa','txt1': 'Web Designer', 'txt2':'Backend Developer' ,'txt3':'Estudiantes del Sena en Análisis y Desarrollo de Sistemas de información Graduados como técnicos en sistemas', 'txt4':'denalme609@gmail.com', 'txt5':'sergiosaldarriagadavila@gmail.com', 'txt6':'lauraagudelo102001@gmail.com', 'txt7':'SENA 2020 ', 'txt8':'Denisse Alejandra Alzate Meneses | Sergio León Saldarriaga Dávila | Laura Vanessa Agudelo Arias' })
+    def get_context_data(self, **kwargs):
+        ctx = super(NosotrosPageView, self).get_context_data(**kwargs)
+        ctx['btn1'] = 'Inicio'
+        ctx['btn2'] = 'Nosotros'
+        ctx['btn3'] = 'Contacto'
+        ctx['btn4'] = 'Presentación'
+        ctx['txt1'] = '¡CONOCE A NUESTROS DESARROLLADORES!'
+        ctx['txt2'] = 'Nosotros'
+        ctx['txt3'] = 'SENA'
+        ctx['txt4'] = 'Denisse Alejandra Alzate Meneses | Sergio León Saldarriaga Dávila | Laura Vanessa Agudelo Arias'
+        return ctx
+
+
 
 class PresentacionPageView(TemplateView):
 
     template_name = "presentacion.html"
 
     def get(self,request, *args, **kwargs):
-        return render(request, self.template_name, {'btn1':'Inicio', 'btn2':'Nosotros', 'btn3':'Contacto', 'btn4': 'Presentación', 'txt1': '¡CONOCE MÁS ACERCA DE SAV!', 'tituloProblema1': 'PROBLEMA Y SOLUCIÓN','spanUno': '1','spanDos': '2', 'tituloProblema': 'PROBLEMA', 'texto1':'Actualmente en la parte administrativa del área de Contrato de Aprendizaje, ubicado en el Centro de Tecnología de la Manufactura Avanzada del SENA, no cuenta con un sistema que permita llevar un control más organizado de los registros y la documentación de los aprendices en cuanto a la alternativa escogida por los mismos para la realización de las prácticas.', 'tituloSolucion': 'SOLUCIÓN', 'texto2':'Debido a la problemática presentada en el área de Contrato de Aprendizaje, el equipo desarrollador decidió elaborar un sistema que ayudará a la organización, es por esto que se desarrollo SAV, un aplicativo web dirigido a los administradores del área de Contrato de Aprendizaje del Centro de la Manufactura Avanzada SENA, este brindará ayuda al área a tener una organización más adecuada de la información de cada aprendiz de acuerdo a la alternativa escogida por los mismos. ', 'tituloFuncion':'FUNCIÓN', 'texto3': 'El sistema se definiría como una plataforma en la cual se podrá conseguir de manera rápida y eficiente la información acerca de la alternativa escogida en la etapa lectiva y productiva de los aprendices.', 'tituloHerramientas':'HERRAMIENTAS UTILIZADAS PARA SU DESARROLLO','tituloUno': 'DOCUMENTACIÓN SAV', 'txt2': 'SENA', 'txt3':'Denisse Alejandra Alzate Meneses | Sergio León Saldarriaga Dávila | Laura Vanessa Agudelo Arias' })
+        return render(request, self.template_name, {'btn1':'Inicio', 'btn2':'Nosotros', 'btn3':'Contacto', 'btn4': 'Presentación', 'txt1': '¡CONOCE MÁS ACERCA DE SAV!', 'tituloProblema1': 'PROBLEMA Y SOLUCIÓN','spanUno': '1','spanDos': '2', 'tituloProblema': 'PROBLEMA', 'texto1':'Actualmente en la parte administrativa del área de Contrato de Aprendizaje, ubicado en el Centro de Tecnología de la Manufactura Avanzada del SENA, no cuenta con un sistema que permita llevar un control más organizado de los registros y la documentación de los aprendices en cuanto a la alternativa escogida por los mismos para la realización de las prácticas.', 'tituloSolucion': 'SOLUCIÓN', 'texto2':'Debido a la problemática presentada en el área de Contrato de Aprendizaje, el equipo desarrollador decidió elaborar un sistema que ayudará a la organización, es por esto que se desarrollo SAV, un aplicativo web dirigido a los administradores del área de Contrato de Aprendizaje del Centro de la Manufactura Avanzada SENA, este brindará ayuda al área a tener una organización más adecuada de la información de cada aprendiz de acuerdo a la alternativa escogida por los mismos. ', 'tituloFuncion':'FUNCIÓN', 'texto3': 'El sistema se definiría como una plataforma en la cual se podrá conseguir de manera rápida y eficiente la información acerca de la alternativa escogida en la etapa lectiva y productiva de los aprendices.', 'tituloHerramientas':'HERRAMIENTAS UTILIZADAS PARA SU DESARROLLO','tituloUno': 'Esquema de la Base de Datos|SAV', 'txt2': 'SENA', 'txt3':'Denisse Alejandra Alzate Meneses | Sergio León Saldarriaga Dávila | Laura Vanessa Agudelo Arias' })
 
 def contacto(request):
         formContact = ContactForm()
